6fe9827ad740ddb83d6d755c9937c05888df08a7
terrioo <50663012+terrioo@users.noreply.github.com>
2019-05-26 20:15:32 +0300

    Add files via upload

---
+++ lesson04/task02/SquareOfCircle.java
@@ -0,0 +1,7 @@
+package lesson04.task02;
+
+public class SquareOfCircle {
+    public static double calculateSquare(double radius){
+        return Math.PI * Math.pow(radius, 2);
+    }
+}
---
+++ lesson04/task02/SquareOfRing.java
@@ -0,0 +1,7 @@
+package lesson04.task02;
+
+public class SquareOfRing {
+    public static double calculateSquare(double square1, double square2) {
+        return square1 - square2;
+    }
+}
