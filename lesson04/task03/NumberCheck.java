9438032d0891e1160c174f22181f3a34397e2e3a
terrioo <50663012+terrioo@users.noreply.github.com>
2019-05-29 13:38:17 +0300

    Delete NumberCheck.java

--- lesson04/task03/NumberCheck.java
+++
@@ -1,9 +0,0 @@
-package lesson04.task03;
-
-public class NumberCheck {
-    public static void main(String[] args) {
-        int a = 1368;
-        System.out.printf("Образуют ли цифры числа %d возрастающую(или убывающую) последовательность? --> %s\n", a,
-                (Check.checkNumbers(a)) == true ? "Да" : "Нет");
-    }
-}
