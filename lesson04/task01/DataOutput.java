198c0e29abad172beaec9e35b2701b6e7de1926f
terrioo <50663012+terrioo@users.noreply.github.com>
2019-05-29 13:33:59 +0300

    Delete DataOutput.java

--- lesson04/task01/DataOutput.java
+++
@@ -1,8 +0,0 @@
-package lesson04.task01;
-
-public class DataOutput {
-    public static void print(double a, double b) {
-        System.out.printf("Are the numbers %.3f, %.3f equal? --> %s\n", a, b,
-                (NumberComparator.compareNumbers(a, b) == true ? "Numbers are equal" : "Numbers are different"));
-    }
-}
