0bf8da97ee632115cece284b24ab5ce592e4d281
terrioo <50663012+terrioo@users.noreply.github.com>
2019-05-29 13:39:57 +0300

    Delete ReverseOfNumber.java

--- lesson04/task05/ReverseOfNumber.java
+++
@@ -1,8 +0,0 @@
-package lesson04.task05;
-
-public class ReverseOfNumber {
-    public static void main(String[] args) {
-        int number = 5789332;
-        System.out.println("Number ia " + number + " Reverse number is " + ReverseOrder.reverse(number));
-    }
-}
