2115c6a771925bc698b5d5e48bee5f3fea61f164
terrioo <50663012+terrioo@users.noreply.github.com>
2019-06-01 15:31:28 +0300

    Update LetterInfo.java

--- lesson05/src/by/epam/javatraining/baklaga/lesson05/task02/LetterInfo.java
+++ lesson05/src/by/epam/javatraining/baklaga/lesson05/task02/LetterInfo.java
@@ -4,5 +4,4 @@ public class LetterInfo {
     public static void print(char letter, boolean result) {
         System.out.printf("Symbol %c is %s \n", letter, (result == true ? "vowel" : "not vowel"));
     }
-
 }
