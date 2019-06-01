4d3e38e1258df51a85fe9d723dcff2cf3fc0fd84
terrioo <50663012+terrioo@users.noreply.github.com>
2019-06-01 15:35:12 +0300

    Add files via upload

---
+++ lesson05/test/by/epam/javatraining/baklaga/lesson05/task03/MoodSensorTest.java
@@ -0,0 +1,43 @@
+package by.epam.javatraining.baklaga.lesson05.task03;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MoodSensorTest {
+
+    @Test
+    public void showMyMood1() {
+        int moodIndicator = 0;
+        String expected = ":(";
+        assertEquals(expected, MoodSensor.showMyMood(moodIndicator));
+    }
+
+    @Test
+    public void showMyMood2() {
+        int moodIndicator = 1;
+        String expected = ":|";
+        assertEquals(expected, MoodSensor.showMyMood(moodIndicator));
+    }
+
+    @Test
+    public void showMyMood3() {
+        int moodIndicator = 2;
+        String expected = ":)";
+        assertEquals(expected, MoodSensor.showMyMood(moodIndicator));
+    }
+
+    @Test
+    public void showMyMood4() {
+        int moodIndicator = 3;
+        String expected = "(-_-)";
+        assertEquals(expected, MoodSensor.showMyMood(moodIndicator));
+    }
+
+    @Test
+    public void showMyMood5() {
+        int moodIndicator = 77;
+        String expected = "(-_-)";
+        assertEquals(expected, MoodSensor.showMyMood(moodIndicator));
+    }
+}
