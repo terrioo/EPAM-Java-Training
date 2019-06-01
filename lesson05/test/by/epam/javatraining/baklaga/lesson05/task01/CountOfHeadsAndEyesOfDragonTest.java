090caed6c397cbbbffefddb4b43d8bd9fb4c8323
terrioo <50663012+terrioo@users.noreply.github.com>
2019-06-01 15:32:57 +0300

    Add files via upload

---
+++ lesson05/test/by/epam/javatraining/baklaga/lesson05/task01/CountOfHeadsAndEyesOfDragonTest.java
@@ -0,0 +1,56 @@
+package by.epam.javatraining.baklaga.lesson05.task01;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CountOfHeadsAndEyesOfDragonTest {
+
+    @Test
+    public void calculateHeadWrongAge() {
+        int age = -10;
+
+        int expected = 0;
+        assertEquals(expected, CountOfHeadsAndEyesOfDragon.calculateHeads(age));
+    }
+
+    @Test
+    public void calculateHeadsAtBirth() {
+        int age = 0;
+
+        int expected = 3;
+        assertEquals(expected, CountOfHeadsAndEyesOfDragon.calculateHeads(age));
+    }
+
+    @Test
+    public void calculateHeadsAtFirstPeriod() {
+        int age = 100;
+
+        int expected = 303;
+        assertEquals(expected, CountOfHeadsAndEyesOfDragon.calculateHeads(age));
+    }
+
+    @Test
+    public void calculateHeadsBetweenFirstAndSecondPeriod() {
+        int age = 250;
+
+        int expected = 703;
+        assertEquals(expected, CountOfHeadsAndEyesOfDragon.calculateHeads(age));
+    }
+
+    @Test
+    public void calculateHeadsAtSecondPeriod() {
+        int age = 350;
+
+        int expected = 853;
+        assertEquals(expected, CountOfHeadsAndEyesOfDragon.calculateHeads(age));
+    }
+
+    @Test
+    public void calculateEyes() {
+        int age = 20;
+
+        int expected = 126;
+        assertEquals(expected, CountOfHeadsAndEyesOfDragon.calculateEyes(age));
+    }
+}
