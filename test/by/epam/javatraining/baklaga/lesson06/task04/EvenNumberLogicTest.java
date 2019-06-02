c07bf4f3426d6c19334031a71d1d005f8975ae7d
terrioo <50663012+terrioo@users.noreply.github.com>
2019-06-03 00:54:05 +0300

    Add files via upload

---
+++ test/by/epam/javatraining/baklaga/lesson06/task04/EvenNumberLogicTest.java
@@ -0,0 +1,29 @@
+package by.epam.javatraining.baklaga.lesson06.task04;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EvenNumberLogicTest {
+
+    @Test
+    public void isNumberEven() {
+        int number = 88_888;
+        boolean expected = true;
+        assertEquals(expected, EvenNumberLogic.isNumberEven(number));
+    }
+
+    @Test
+    public void isNumberOdd() {
+        int number = 1_372_847;
+        boolean expected = false;
+        assertEquals(expected, EvenNumberLogic.isNumberOdd(number));
+    }
+
+    @Test
+    public void isAllNumbersEvenOrOdd() {
+        int number = 288_644;
+        boolean expected = true;
+        assertEquals(expected, EvenNumberLogic.isAllNumbersEvenOrOdd(number));
+    }
+}
