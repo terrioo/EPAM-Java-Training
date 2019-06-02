b16ed505126bb011529916c8a1fb406677ece4c2
terrioo <50663012+terrioo@users.noreply.github.com>
2019-06-03 00:51:40 +0300

    Add files via upload

---
+++ test/by/epam/javatraining/baklaga/lesson06/task01/RandomCoinTest.java
@@ -0,0 +1,23 @@
+package by.epam.javatraining.baklaga.lesson06.task01;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RandomCoinTest {
+
+    @Test
+    public void throwCoinFindHeadsMistakeCount() {
+        int count = -2;
+        int expected = 0;
+        assertEquals(expected, RandomCoin.throwCoinFindHeads(count));
+    }
+
+    @Test
+    public void findTails() {
+        int count = 1000;
+        int countHeads = 555;
+        int expected = 445;
+        assertEquals(expected, RandomCoin.FindTails(count, countHeads));
+    }
+}
