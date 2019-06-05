47b3c954f7d34ae04d8d710cb07076c8331ca444
terrioo <50663012+terrioo@users.noreply.github.com>
2019-06-05 14:31:26 +0300

    Update Vowel.java

--- lesson05/src/by/epam/javatraining/baklaga/lesson05/task02/Vowel.java
+++ lesson05/src/by/epam/javatraining/baklaga/lesson05/task02/Vowel.java
@@ -1,8 +1,16 @@
 package by.epam.javatraining.baklaga.lesson05.task02;
 
 public class Vowel {
-    public static void main(String[] args) {
-        char letter = 'я';
+   public static void main(String[] args) {
+        char letter = 'o';
         LetterInfo.print(letter, VowelCheck.isVowel(letter));
+        LetterInfo.print(letter, VowelCheck.isVowel1(letter));
+        LetterInfo.print(letter, VowelCheck.isVowel2(letter));
+        LetterInfo.print(letter, VowelCheck.isVowel3(letter));
+        LetterInfo.print(letter, VowelCheck.isVowel4(letter));
+        LetterInfo.print(letter, VowelCheck.isVowel5(letter));
+        LetterInfo.print(letter, VowelCheck.isVowel6(letter));
+        LetterInfo.print(letter, VowelCheck.isVowel7(letter));
+        LetterInfo.print(letter, VowelCheck.isVowel8(letter));
     }
 }
