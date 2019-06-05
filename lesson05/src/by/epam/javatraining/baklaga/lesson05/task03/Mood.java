aeaa1e55ed831c9dfe70223b63598f777cc60f0c
terrioo <50663012+terrioo@users.noreply.github.com>
2019-06-05 13:32:16 +0300

    Update Mood.java

--- lesson05/src/by/epam/javatraining/baklaga/lesson05/task03/Mood.java
+++ lesson05/src/by/epam/javatraining/baklaga/lesson05/task03/Mood.java
@@ -3,9 +3,7 @@ package by.epam.javatraining.baklaga.lesson05.task03;
 import java.util.Random;
 
 public class Mood {
-    public static void main(String[] args) {
-        Random random = new Random();
-        int moodIndicator = random.nextInt(3);
-        MoodInfo.print(MoodSensor.showMyMood(moodIndicator));
+   public static void main(String[] args) {
+        MoodInfo.print(MoodSensor.showMyMood());
     }
 }
