00695e8097103e1b1cf92a40f1e1b563d77d48a8
terrioo <50663012+terrioo@users.noreply.github.com>
2019-06-05 13:33:12 +0300

    Update MoodSensor.java

--- lesson05/src/by/epam/javatraining/baklaga/lesson05/task03/MoodSensor.java
+++ lesson05/src/by/epam/javatraining/baklaga/lesson05/task03/MoodSensor.java
@@ -1,17 +1,26 @@
 package by.epam.javatraining.baklaga.lesson05.task03;
 
 
+import java.util.Random;
+
 public class MoodSensor {
-    public static String showMyMood(int moodIndicator) {
-        switch (moodIndicator) {
+    public static final Random random = new Random();
+    public static final int MOOD_INDICATOR = 4;
+    public static final String SAD = ":(";
+    public static final String NEUTRAL = ":)";
+    public static final String SO_SO = "(-_-)";
+    public static final String SMILE = ":)";
+
+    public static String showMyMood() {
+        String mood = NEUTRAL;
+        switch (random.nextInt(MOOD_INDICATOR)) {
             case 0:
-                return ":(";
-            case 1:
-                return ":|";
+                mood = SAD;
             case 2:
-                return ":)";
-            default:
-                return "(-_-)";
+                mood = SO_SO;
+            case 3:
+                mood = SMILE;
         }
+        return mood;
     }
 }
