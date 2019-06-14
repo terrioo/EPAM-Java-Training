02f84253ec419b7a5e547247bc70ed73c1bb1fd0
terrioo <50663012+terrioo@users.noreply.github.com>
2019-06-14 19:15:34 +0300

    Update PrimeNumberCheck.java

--- src/by/epam/javatraining/baklaga/lesson06/task05/PrimeNumberCheck.java
+++ src/by/epam/javatraining/baklaga/lesson06/task05/PrimeNumberCheck.java
@@ -2,17 +2,18 @@ package by.epam.javatraining.baklaga.lesson06.task05;
 
 public class PrimeNumberCheck {
     public static boolean isNumberPrime(int number) {
+        boolean result = true;
         if (number > 0) {
             int numberEqual = number;
             int startingPoint = 0;
             while (startingPoint < number && numberEqual > 2) {
                 if ((number % (numberEqual - 1)) == 0) {
-                    return false;
+                    result = false;
                 }
                 numberEqual--;
                 startingPoint++;
             }
-        }
-        return true;
+        } else result = false;
+        return result;
     }
 }
