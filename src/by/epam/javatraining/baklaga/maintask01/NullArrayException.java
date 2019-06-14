064cac8b35b192c6fa8471248c2d249badf1b7a7
terrioo <50663012+terrioo@users.noreply.github.com>
2019-06-14 17:35:58 +0300

    Update NullArrayException.java

--- src/by/epam/javatraining/baklaga/maintask01/NullArrayException.java
+++ src/by/epam/javatraining/baklaga/maintask01/NullArrayException.java
@@ -1,7 +1,12 @@
 package by.epam.javatraining.baklaga.maintask01;
 
+import org.apache.log4j.Logger;
+
 public class NullArrayException extends Exception {
+    private static final Logger LOGGER = Logger.getRootLogger();
+
     public NullArrayException(String message) {
         super(message);
+        LOGGER.warn("Vector is null. It's length must be greater than 0.");
     }
 }
