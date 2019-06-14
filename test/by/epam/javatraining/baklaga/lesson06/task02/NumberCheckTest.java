722b8cb95d8d9faf540cf12eecac7d62cbf16075
terrioo <50663012+terrioo@users.noreply.github.com>
2019-06-14 19:07:39 +0300

    Update NumberCheckTest.java

--- test/by/epam/javatraining/baklaga/lesson06/task02/NumberCheckTest.java
+++ test/by/epam/javatraining/baklaga/lesson06/task02/NumberCheckTest.java
@@ -21,6 +21,27 @@ public class NumberCheckTest {
     }
 
     @Test
+    public void isSequenceRiseZero() {
+        int number = 0;
+        boolean expected = false;
+        assertEquals(expected, NumberCheck.isSequenceRise(number));
+    }
+
+    @Test
+    public void isSequenceRiseNegativeRight() {
+        int number = -1369;
+        boolean expected = true;
+        assertEquals(expected, NumberCheck.isSequenceRise(number));
+    }
+
+    @Test
+    public void isSequenceNegativeWrong() {
+        int number = -848_327;
+        boolean expected = false;
+        assertEquals(expected, NumberCheck.isSequenceRise(number));
+    }
+
+    @Test
     public void isSequenceDownRight() {
         int number = 987_654_321;
         boolean expected = true;
@@ -30,6 +51,27 @@ public class NumberCheckTest {
     @Test
     public void isSequenceDownWrong() {
         int number = 7_932_694;
+        boolean expected = false;
+        assertEquals(expected, NumberCheck.isSequenceDown(number));
+    }
+
+    @Test
+    public void isSequenceDowZeronWrong() {
+        int number = 0;
+        boolean expected = false;
+        assertEquals(expected, NumberCheck.isSequenceDown(number));
+    }
+
+    @Test
+    public void isSequenceDownNegativeRight() {
+        int number = -987_654_321;
+        boolean expected = true;
+        assertEquals(expected, NumberCheck.isSequenceDown(number));
+    }
+
+    @Test
+    public void isSequenceDownNegativeWrong() {
+        int number = -7_932_694;
         boolean expected = false;
         assertEquals(expected, NumberCheck.isSequenceDown(number));
     }
