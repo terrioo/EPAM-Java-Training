cc8a16efae0cb4cce504f9a684a033262824e437
terrioo <50663012+terrioo@users.noreply.github.com>
2019-06-14 19:12:20 +0300

    Update NumberCheck.java

--- src/by/epam/javatraining/baklaga/lesson06/task02/NumberCheck.java
+++ src/by/epam/javatraining/baklaga/lesson06/task02/NumberCheck.java
@@ -3,43 +3,39 @@ package by.epam.javatraining.baklaga.lesson06.task02;
 public class NumberCheck {
     public static boolean isSequenceRise(int number) {
         boolean rise = true;
-        number = Math.abs(number);
-        int lastNumber = number % 10;
-        number /= 10;
+        if (number != 0) {
+            number = Math.abs(number);
+            int lastNumber = number % 10;
 
-        while (number > 0) {
-            int previousNumber = number % 10;
-            if (lastNumber > previousNumber) {
-                lastNumber = previousNumber;
-                number /= 10;
-            } else {
-                rise = false;
-                break;
+            while ((number /= 10) > 0) {
+                int previousNumber = number % 10;
+                if (lastNumber > previousNumber) {
+                    lastNumber = previousNumber;
+                } else {
+                    rise = false;
+                    break;
+                }
             }
-        }
+        } else rise = false;
         return rise;
     }
 
     public static boolean isSequenceDown(int number) {
         boolean down = true;
-        number = Math.abs(number);
-        int lastNumber = number % 10;
-        number /= 10;
+        if (number != 0) {
+            number = Math.abs(number);
+            int lastNumber = number % 10;
 
-        while (number > 0) {
-            int previousNumber = number % 10;
-            if (lastNumber < previousNumber) {
-                lastNumber = previousNumber;
-                number /= 10;
-            } else {
-                down = false;
-                break;
+            while ((number /= 10) > 0) {
+                int previousNumber = number % 10;
+                if (lastNumber < previousNumber) {
+                    lastNumber = previousNumber;
+                } else {
+                    down = false;
+                    break;
+                }
             }
-        }
+        } else down = false;
         return down;
     }
 }
-
-
-
-
