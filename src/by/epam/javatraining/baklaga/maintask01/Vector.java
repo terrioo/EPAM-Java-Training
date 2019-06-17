1e85e3d1d312080f3d2c3dc7eede56c5831acbaa
terrioo <50663012+terrioo@users.noreply.github.com>
2019-06-17 15:30:15 +0300

    Update Vector.java

--- src/by/epam/javatraining/baklaga/maintask01/Vector.java
+++ src/by/epam/javatraining/baklaga/maintask01/Vector.java
@@ -20,71 +20,57 @@ package by.epam.javatraining.baklaga.maintask01;
 public class Vector {
     public static void main(String[] args) throws NullArrayException {
         double[] vector = new double[10];
-        vector = VectorLogic.fillArray(vector);     // fill array with randomized values;
+        vector = Initializer.fillArray(vector);     // fill array with randomized values;
         double number = 6.0;
-        boolean nullVector = VectorLogic.checkNull(vector);
-        if (nullVector != true) {
-            Printer.printArray(vector);
+        boolean sortIndicator = false;  // true - increase sort; false - decrease sort;
+        Printer.printArray(vector);
 
-            double maxValue = VectorLogic.findMaxValue(vector);
-            Printer.print("Max value is " + maxValue);
+        double maxValue = VectorLogic.findMaxValue(vector);
+        Printer.print("Max value is " + maxValue);
 
-            double minValue = VectorLogic.findMinValue(vector);
-            Printer.print("Min value is " + minValue);
+        double minValue = VectorLogic.findMinValue(vector);
+        Printer.print("Min value is " + minValue);
 
-            double arithmeticAverage = VectorLogic.calculateArithmeticAverage(vector);
-            Printer.print("ArithmeticAverage is " + arithmeticAverage);
+        double arithmeticAverage = VectorLogic.calculateArithmeticAverage(vector);
+        Printer.print("ArithmeticAverage is " + arithmeticAverage);
 
-            double geometricMean = VectorLogic.calculateGeometricMean(vector);
-            Printer.print("GeometricMean is " + geometricMean);
+        double geometricMean = VectorLogic.calculateGeometricMean(vector);
+        Printer.printGeometricMean(geometricMean);
 
-            Printer.isArraySorted(VectorLogic.isArraySorted(vector));
+        Printer.isArraySorted(VectorLogic.isArraySorted(vector));
 
-            double localMaximum = VectorLogic.findIndexOfLocalMax(vector);
-            Printer.print("Index of local maximum is " + localMaximum);
+        double localMaximum = VectorLogic.findIndexOfLocalMax(vector);
+        Printer.print("Index of local maximum is " + localMaximum);
 
-            double localMinimum = VectorLogic.findIndexOfLocalMin(vector);
-            Printer.print("Index of local minimum is " + localMinimum);
+        double localMinimum = VectorLogic.findIndexOfLocalMin(vector);
+        Printer.print("Index of local minimum is " + localMinimum);
 
-            double[] reverse = VectorLogic.reverseVector(vector);
-            Printer.printArray(reverse);
+        double[] reverse = VectorLogic.reverseVector(vector);
+        Printer.printArray(reverse);
 
-            int resultOfLinearSearchNumber = VectorLogic.linearSearch(vector, number);
-            Printer.printResultOfSearch(resultOfLinearSearchNumber);
+        int resultOfLinearSearchNumber = VectorLogic.linearSearch(vector, number);
+        Printer.printResultOfSearch(resultOfLinearSearchNumber);
 
-            double[] bubbleSortIncrease = VectorLogic.sortBubbleIncrease(vector);
-            Printer.printArray(bubbleSortIncrease);
+        double[] bubbleSortIncrease = VectorLogic.sortBubble(vector, true);
+        Printer.printArray(bubbleSortIncrease);
 
-            double[] bubbleSortDecrease = VectorLogic.sortBubbleDecrease(vector);
-            Printer.printArray(bubbleSortDecrease);
+        // Binary algorithm can use only for sorted massive;
+        int resultOfBinarySearchNumber = VectorLogic.binarySearch(bubbleSortIncrease, number);
+        Printer.printResultOfSearch(resultOfBinarySearchNumber);
 
-            // Binary algorithm can use only for sorted massive;
-            int resultOfBinarySearchNumber = VectorLogic.binarySearch(bubbleSortDecrease, number);
-            Printer.printResultOfSearch(resultOfBinarySearchNumber);
+        double[] bubbleSort = VectorLogic.sortBubble(vector, sortIndicator);
+        Printer.printArray(bubbleSort);
 
-            double[] insertionSortIncrease = VectorLogic.sortInsertionIncrease(vector);
-            Printer.printArray(insertionSortIncrease);
+        double[] insertionSort = VectorLogic.sortInsertion(vector, sortIndicator);
+        Printer.printArray(insertionSort);
 
-            double[] insertionSortDecrease = VectorLogic.sortInsertionDecrease(vector);
-            Printer.printArray(insertionSortDecrease);
+        double[] selectionSort = VectorLogic.sortSelection(vector, sortIndicator);
+        Printer.printArray(selectionSort);
 
-            double[] selectionSortIncrease = VectorLogic.sortSelectionIncrease(vector);
-            Printer.printArray(selectionSortIncrease);
+        double[] quickSort = VectorLogic.sortQuick(vector, sortIndicator);
+        Printer.printArray(quickSort);
 
-            double[] selectionSortDecrease = VectorLogic.sortSelectionDecrease(vector);
-            Printer.printArray(selectionSortDecrease);
-
-            double[] quickSortIncrease = VectorLogic.sortQuickIncrease(vector);
-            Printer.printArray(quickSortIncrease);
-
-            double[] quickSortDecrease = VectorLogic.sortQuickDecrease(vector);
-            Printer.printArray(quickSortDecrease);
-
-            double[] mergeSortIncrease = VectorLogic.sortMergeIncrease(vector);
-            Printer.printArray(mergeSortIncrease);
-
-            double[] mergeSortDecrease = VectorLogic.sortMergeDecrease(vector);
-            Printer.printArray(mergeSortDecrease);
-        } else throw new NullArrayException("Vector can't be null. It's length must be greater than 0.");
+        double[] mergeSort = VectorLogic.sortMerge(vector, sortIndicator);
+        Printer.printArray(mergeSort);
     }
 }
