0c4e9686ed0b452ed23eaf904868f71d25cabdf7
terrioo <50663012+terrioo@users.noreply.github.com>
2019-06-17 11:42:09 +0300

    Update FactorialLogic.java

--- src/by/epam/javatraining/baklaga/lesson06/task03/FactorialLogic.java
+++ src/by/epam/javatraining/baklaga/lesson06/task03/FactorialLogic.java
@@ -1,16 +1,14 @@
 package by.epam.javatraining.baklaga.lesson06.task03;
 
 public class FactorialLogic {
-    public static int factorial;
-
     public static int findFactorial(int number) {
-        if (number >= 0) {
-            factorial = 1;
-            int i = 1;
-            while (i <= number) {
-                factorial *= number;
-                number -= 1;
-            }
+        int factorial;
+        if (number < 0) {
+            factorial = 0;
+        }
+        factorial = 1;
+        for (int i = 2; i <= number; i++) {
+            factorial *= i;
         }
         return factorial;
     }
