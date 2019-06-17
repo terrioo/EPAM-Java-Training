5e4eae93d79f3a144aedc87bc2bcb1804a49d417
terrioo <50663012+terrioo@users.noreply.github.com>
2019-06-17 15:31:05 +0300

    Update VectorLogic.java

--- src/by/epam/javatraining/baklaga/maintask01/VectorLogic.java
+++ src/by/epam/javatraining/baklaga/maintask01/VectorLogic.java
@@ -1,29 +1,20 @@
 package by.epam.javatraining.baklaga.maintask01;
 
-import java.util.Random;
 
 import org.apache.log4j.Logger;
 
 public class VectorLogic {
     private static final Logger LOGGER = Logger.getRootLogger();
-    public static final Random random = new Random();
-    public static final int RANDOM_INDICATOR = 16;
     public static final int NO_RESULT = -1;
     public static final int HALF = 2;
 
-    // Fill the array with random values;
-    public static double[] fillArray(double[] vector) {
-        LOGGER.debug("Fill the array with random values");
-        for (int i = 0; i < vector.length; i++) {
-            vector[i] = random.nextInt(RANDOM_INDICATOR);
-        }
-        return vector;
-    }
 
     // Check is vector null array or not;
-    public static boolean checkNull(double[] vector) {
+    public static void checkNull(double[] vector) throws NullArrayException {
         LOGGER.debug("Check is vector null array or not");
-        return vector.length == 0 || vector.length == 1;
+        if (vector.length == 0 || vector == null) {
+            throw new NullArrayException("Vector can't be null. It's length must be greater than 0.");
+        }
     }
 
     // Swapping elements of array;
@@ -34,49 +25,65 @@ public class VectorLogic {
     }
 
     // Find the maximum value of vector;
-    public static double findMaxValue(double[] vector) {
+    public static double findMaxValue(double[] vector) throws NullArrayException {
+        checkNull(vector);
         LOGGER.debug("Find the maximum value of vector");
         double max = vector[0];
         for (int i = 1; i < vector.length; i++) {
+            LOGGER.trace("Find the maximum value of vector. Iteration step: " + i);
             max = max < vector[i] ? vector[i] : max;
         }
         return max;
     }
 
     // Find the minimum value of vector;
-    public static double findMinValue(double[] vector) {
+    public static double findMinValue(double[] vector) throws NullArrayException {
+        checkNull(vector);
         LOGGER.debug("Find the minimum value of vector");
         double min = vector[0];
         for (int i = 1; i < vector.length; i++) {
+            LOGGER.trace("Find the minimum value of vector. Iteration step: " + i);
             min = min > vector[i] ? vector[i] : min;
         }
         return min;
     }
 
     // Find arithmetic average;
-    public static double calculateArithmeticAverage(double[] vector) {
+    public static double calculateArithmeticAverage(double[] vector) throws NullArrayException {
+        checkNull(vector);
         LOGGER.debug("Find arithmetic average");
         double sumOfValues = 0;
         for (int i = 0; i < vector.length; i++) {
+            LOGGER.trace("Find arithmetic average. Iteration step: " + i);
             sumOfValues += vector[i];
         }
         return sumOfValues /= vector.length;
     }
 
     // Find geometric mean;
-    public static double calculateGeometricMean(double[] vector) {
+    public static double calculateGeometricMean(double[] vector) throws NullArrayException {
+        checkNull(vector);
         LOGGER.debug("Find geometric mean");
         double multiplication = 1.0;
+        double geometricMean;
         for (int i = 0; i < vector.length; i++) {
+            LOGGER.trace("Find geometric mean. Iteration step: " + i);
+            if (vector[i] <= 0) {
+                geometricMean = NO_RESULT;
+                break;
+            }
             multiplication *= vector[i];
         }
-        return Math.pow(multiplication, 1.0 / vector.length);
+        geometricMean = Math.pow(multiplication, 1.0 / vector.length);
+        return geometricMean;
     }
 
     // Check is the array sorted;
-    public static boolean isSequenceRise(double[] vector) {
+    public static boolean isSequenceRise(double[] vector) throws NullArrayException {
+        checkNull(vector);
         LOGGER.debug("Check is the array sorted - is sequence rise");
         for (int i = 0; i < vector.length - 1; i++) {
+            LOGGER.trace("Check is the array sorted - is sequence rise. Iteration step: " + i);
             if (!(vector[i] < vector[i + 1])) {
                 return false;
             }
@@ -84,9 +91,11 @@ public class VectorLogic {
         return true;
     }
 
-    public static boolean isSequenceDown(double[] vector) {
+    public static boolean isSequenceDown(double[] vector) throws NullArrayException {
+        checkNull(vector);
         LOGGER.debug("Check is the array sorted - is sequence down");
         for (int i = 0; i < vector.length - 1; i++) {
+            LOGGER.trace("Check is the array sorted - is sequence down. Iteration step: " + i);
             if (!(vector[i] > vector[i + 1])) {
                 return false;
             }
@@ -94,16 +103,18 @@ public class VectorLogic {
         return true;
     }
 
-    public static boolean isArraySorted(double[] vector) {
-
+    public static boolean isArraySorted(double[] vector) throws NullArrayException {
+        checkNull(vector);
         LOGGER.debug("Check is the array sorted");
         return isSequenceRise(vector) || isSequenceDown(vector);
     }
 
     // Find the index of local maximum;
-    public static int findIndexOfLocalMax(double[] vector) {
+    public static int findIndexOfLocalMax(double[] vector) throws NullArrayException {
+        checkNull(vector);
         LOGGER.debug("Find the index of local maximum");
         for (int i = 0; i < vector.length; ++i) {
+            LOGGER.trace("Find the index of local maximum. Iteration step: " + i);
             if ((i == 0 && vector[i] > vector[i + 1])
                     || (i == vector.length - 1 && vector[i - 1] < vector[i])
                     || (i != 0 && i != vector.length - 1 && vector[i] > vector[i - 1] && vector[i] > vector[i + 1])) {
@@ -114,9 +125,11 @@ public class VectorLogic {
     }
 
     // Find the index of local minimum;
-    public static int findIndexOfLocalMin(double[] vector) {
+    public static int findIndexOfLocalMin(double[] vector) throws NullArrayException {
+        checkNull(vector);
         LOGGER.debug("Find the index of local minimum");
         for (int i = 0; i < vector.length; ++i) {
+            LOGGER.trace("Find the index of local minimum. Iteration step: " + i);
             if ((i == 0 && vector[i] < vector[i + 1])
                     || (i == vector.length - 1 && vector[i - 1] > vector[i])
                     || (i != 0 && i != vector.length - 1 && vector[i] < vector[i - 1] && vector[i] < vector[i + 1])) {
@@ -127,9 +140,11 @@ public class VectorLogic {
     }
 
     // Reverse array;
-    public static double[] reverseVector(double[] vector) {
+    public static double[] reverseVector(double[] vector) throws NullArrayException {
+        checkNull(vector);
         LOGGER.debug("Reverse array");
         for (int i = 0; i < vector.length / HALF; i++) {
+            LOGGER.trace("Reverse array. Iteration step: " + i);
             exchangeElements(vector, i, vector.length - 1 - i);
         }
         return vector;
@@ -138,9 +153,11 @@ public class VectorLogic {
     // Search the value among values of vector;
 
     // Use  linear algorithm;
-    public static int linearSearch(double[] vector, double number) {
+    public static int linearSearch(double[] vector, double number) throws NullArrayException {
+        checkNull(vector);
         LOGGER.debug("Search the value among values of vector by linear algorithm");
         for (int i = 0; i < vector.length; i++) {
+            LOGGER.trace("Search the value among values of vector by linear algorithm. Iteration step: " + i);
             if (vector[i] == number) {
                 return i;
             }
@@ -149,14 +166,17 @@ public class VectorLogic {
     }
 
     // Use  binary algorithm (can use only for sorted massive);
-    public static int binarySearch(double[] vector, double number) {
+    public static int binarySearch(double[] vector, double number) throws NullArrayException {
+        checkNull(vector);
         LOGGER.debug("Search the value among values of vector by binary algorithm");
-        sortBubbleIncrease(vector);
+        sortBubble(vector, true);
         int firstIndex = 0;
         int lastIndex = vector.length - 1;
         int middleIndex;
+        int step = 0;
 
         while (firstIndex <= lastIndex) {
+            LOGGER.trace("Search the value among values of vector by binary algorithm: " + step++);
             middleIndex = (firstIndex + lastIndex) / HALF;
             if (vector[middleIndex] == number) {
                 return middleIndex;
@@ -169,100 +189,81 @@ public class VectorLogic {
         return NO_RESULT;
     }
 
+    // Sort array: sortIndicator demonstrate will be array sort increase or decrease;
+
     // Bubble sort;
-    public static double[] sortBubbleIncrease(double[] vector) {
-        LOGGER.debug("Bubble sort: increase");
+    public static double[] sortBubble(double[] vector, boolean sortIndicator) throws NullArrayException {
+        checkNull(vector);
+        LOGGER.debug("Bubble sort");
         for (int i = 1; i < vector.length; ++i) {
             for (int j = 0; j < vector.length - i; ++j) {
-                if (vector[j] > vector[j + 1]) {
-                    exchangeElements(vector, j, j + 1);
+                LOGGER.trace("Bubble sort. Iteration step: " + i);
+                if (sortIndicator) {
+                    if (vector[j] > vector[j + 1]) {
+                        exchangeElements(vector, j, j + 1);
+                    }
+                } else {
+                    if (vector[j] < vector[j + 1]) {
+                        exchangeElements(vector, j, j + 1);
+                    }
                 }
             }
         }
         return vector;
     }
 
-    public static double[] sortBubbleDecrease(double[] vector) {
-        LOGGER.debug("Bubble sort: decrease");
-        for (int i = 1; i < vector.length; ++i) {
-            for (int j = 0; j < vector.length - i; ++j) {
-                if (vector[j] < vector[j + 1]) {
-                    exchangeElements(vector, j, j + 1);
-                }
-            }
-        }
-        return vector;
-    }
 
     // Insertion sort;
-    public static double[] sortInsertionIncrease(double[] vector) {
-        LOGGER.debug("Insertion sort: increase");
+    public static double[] sortInsertion(double[] vector, boolean sortIndicator) throws NullArrayException {
+        checkNull(vector);
+        LOGGER.debug("Insertion sort");
         for (int left = 0; left < vector.length; left++) {
+            LOGGER.trace("Insertion sort. Iteration step: " + left);
             double value = vector[left];
             int i = left - 1;
             for (; i >= 0; i--) {
-                if (value < vector[i]) {
-                    vector[i + 1] = vector[i];
+                if (sortIndicator) {
+                    if (value < vector[i]) {
+                        vector[i + 1] = vector[i];
+                    } else {
+                        break;
+                    }
                 } else {
-                    break;
+                    if (value > vector[i]) {
+                        vector[i + 1] = vector[i];
+                    } else {
+                        break;
+                    }
                 }
             }
             vector[i + 1] = value;
         }
         return vector;
     }
-
-    public static double[] sortInsertionDecrease(double[] vector) {
-        LOGGER.debug("Insertion sort: decrease");
-        for (int left = 0; left < vector.length; left++) {
-            double value = vector[left];
-            int i = left - 1;
-            for (; i >= 0; i--) {
-                if (value > vector[i]) {
-                    vector[i + 1] = vector[i];
-                } else {
-                    break;
-                }
-            }
-            vector[i + 1] = value;
-        }
-        return vector;
-    }
-
 
     // Selection sort;
-    public static double[] sortSelectionIncrease(double[] vector) {
-        LOGGER.debug("Selection sort: increase");
+    public static double[] sortSelection(double[] vector, boolean sortIndicator) throws NullArrayException {
+        checkNull(vector);
+        LOGGER.debug("Selection sort");
         for (int i = 0; i < vector.length; i++) {
-            int minIndex = i;
+            int index = i;
             for (int j = i; j < vector.length; j++) {
-                if (vector[j] < vector[minIndex]) {
-                    minIndex = j;
+                LOGGER.trace("Selection sort. Iteration step: " + i);
+                if (sortIndicator) {
+                    if (vector[j] < vector[index]) {
+                        index = j;
+                    }
+                } else {
+                    if (vector[j] > vector[index]) {
+                        index = j;
+                    }
                 }
+                exchangeElements(vector, i, index);
             }
-            exchangeElements(vector, i, minIndex);
         }
-
         return vector;
     }
 
-    public static double[] sortSelectionDecrease(double[] vector) {
-        LOGGER.debug("Selection sort: decrease");
-
-        for (int i = 0; i < vector.length; i++) {
-
-            int maxIndex = i;
-
-            for (int j = i; j < vector.length; j++) {
-                if (vector[j] > vector[maxIndex]) {
-                    maxIndex = j;
-                }
-            }
-            exchangeElements(vector, i, maxIndex);
-        }
-
-        return vector;
-    }
 
     // Quick sort;
     private static void sortQuick(double[] vector, int left, int right, boolean order) {
@@ -293,17 +294,18 @@ public class VectorLogic {
         }
     }
 
-    public static double[] sortQuickIncrease(double[] vector) {
+    public static double[] sortQuick(double[] vector, boolean sortIndicator) throws NullArrayException {
+        checkNull(vector);
         LOGGER.debug("Quick sort: increase");
-        sortQuick(vector, 0, vector.length - 1, true);
+        if (sortIndicator) {
+            sortQuick(vector, 0, vector.length - 1, true);
+        } else {
+            sortQuick(vector, 0, vector.length - 1, false);
+        }
+
         return vector;
     }
 
-    public static double[] sortQuickDecrease(double[] vector) {
-        LOGGER.debug("Quick sort: decrease");
-        sortQuick(vector, 0, vector.length - 1, false);
-        return vector;
-    }
 
     // Merge sort;
     private static void sortMerge(double[] vector, int left, int right, boolean order) {
@@ -332,15 +334,14 @@ public class VectorLogic {
         }
     }
 
-    public static double[] sortMergeIncrease(double[] vector) {
-        LOGGER.debug("Merge sort: increase");
-        sortMerge(vector, 0, vector.length - 1, true);
-        return vector;
-    }
-
-    public static double[] sortMergeDecrease(double[] vector) {
-        LOGGER.debug("Merge sort: decrease");
-        sortMerge(vector, 0, vector.length - 1, false);
+    public static double[] sortMerge(double[] vector, boolean sortIndicator) throws NullArrayException {
+        checkNull(vector);
+        LOGGER.debug("Merge sort");
+        if (sortIndicator) {
+            sortMerge(vector, 0, vector.length - 1, true);
+        } else {
+            sortMerge(vector, 0, vector.length - 1, false);
+        }
         return vector;
     }
 }
